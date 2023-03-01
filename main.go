df26c1cf86423f03c091b05a2f43603d34e13f46
ODawah <omar.adel@factmata.com>
2023-03-01 19:43:06 +0200

    hotfix

--- main.go
+++ main.go
@@ -8,34 +8,23 @@ import (
 
 var wg sync.WaitGroup
 
-func inputWriter(inCh chan<- string) {
+func inputWriter(inCh *chan string) {
 	for i := 0; i < 1000; i++ {
 		time.Sleep(10 * time.Millisecond)
-		inCh <- fmt.Sprintf("String %d", i)
+		*inCh <- fmt.Sprintf("String %d", i)
 	}
-	wg.Done()
 }
 
-func reader(outCh <-chan string) {
-	defer wg.Done()
-	for v := range outCh {
-		wg.Add(1)
-		go func() {
-			fmt.Println(v)
-			wg.Done()
-		}()
-	}
+func send(msg string) {
+	fmt.Println(msg)
 }
 
 func main() {
 	start := time.Now()
 
-	ch := make(chan string, 4)
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		go reader(ch)
-	}
-	inputWriter(ch)
+	ch := make(chan string)
+	go inputWriter(&ch)
+
 	close(ch)
 	wg.Wait()
 	end := time.Since(start)
